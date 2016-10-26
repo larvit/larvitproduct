04084efc22444e386c8ae941d3f820fc089bce6c
lilleman <lilleman@larvit.se>
2016-10-26 16:40:27 +0200

    Added debug log to see what takes time on Travis

--- test/00test.js
+++ test/00test.js
@@ -14,7 +14,7 @@ let	productLib;
 
 // Set up winston
 log.remove(log.transports.Console);
-/** /log.add(log.transports.Console, {
+/**/log.add(log.transports.Console, {
 	'level':	'debug',
 	'colorize':	true,
 	'timestamp':	true,
